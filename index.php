e3f30b58ec4667449e3b0f6d276757fe2ddd447a
Unknown <tuffbard@yopmail.fr>
2018-04-07 14:37:53 +0200

    Ajout class chrono

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title>JSGameBook</title>
     <link rel="stylesheet" href="css/main.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.0/moment.min.js"></script>
+    <script src="js/class/chrono.js"></script>
     <script src="js/class/keyboard.js"></script>
     <script src="js/class/player.js"></script>
     <script src="js/main.js"></script>
---
+++ js/class/chrono.js
@@ -0,0 +1,39 @@
+/**
+ * Classe de gestion de chronomètre
+ * @param 
+ */
+class Chrono {
+    constructor(){
+        this.StartDate;
+    }
+
+    /**
+     * Lance le chronomètre
+     */
+    Start(){
+        this.StartDate = moment();
+    }
+
+    /**
+     * Obtient le 
+     */
+    Get(){
+        return moment().diff(this.StartDate, "seconds", true);
+    }
+
+    /**
+     * Remet le chrono à zero
+     */
+    Off(){
+        this.StartDate = undefined;
+    }
+
+    /**
+     * Vérifie si le temps est dépassé
+     * @param {double} seconds 
+     * @return bool
+     */
+    GreaterOrEquals(seconds){
+        return this.Get() >= seconds;
+    }
+}
